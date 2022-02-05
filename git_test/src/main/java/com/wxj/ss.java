7e45081bdace7f8c997cd29ec3c2ea8770fcb76d
wxj <wxj@QQ.com>
2022-02-05 18:52:47 +0800

    third commit

--- git_test/src/main/java/com/wxj/ss.java
+++ git_test/src/main/java/com/wxj/ss.java
@@ -4,5 +4,8 @@ public class ss {
     public static void main(String[] args) {
         System.out.println("git and idea");
         System.out.println("git and idea 2");
+        System.out.println("git and idea 3");
+
+
     }
 }
